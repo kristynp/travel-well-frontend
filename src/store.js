a91c0aa6aa15018ddc9a722605a5b6e7237d92b4
github username <github email address>
2020-06-17 14:18:44 -0600

    added currentUser reducer, and imported to store

--- src/store.js
+++ src/store.js
@@ -1,11 +1,13 @@
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import usersReducer from './reducers/users';
+import currentUser from './reducers/currentUser';
 import thunk from 'redux-thunk';
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const reducer = combineReducers({
-  // each property comes from the name of a reducer
-  users: usersReducer //should be shortened to just be users because the name is the same
+  // each property comes from the name of a reducer //? displaying different syntax options here.
+  users: usersReducer, //should be shortened to just be users because the name is the same
+  currentUser
 })
 const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)))
 
