ad985d9e8e347f87e67d0fc7ada5f11dd0bfb60e
github username <github email address>
2020-06-17 15:48:10 -0600

    created loginForm action and reducer, and imported them to store.js. created a Login component file

--- src/store.js
+++ src/store.js
@@ -1,13 +1,15 @@
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import usersReducer from './reducers/users';
 import currentUser from './reducers/currentUser';
+import loginForm from './reducers/loginForm';
 import thunk from 'redux-thunk';
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const reducer = combineReducers({
   // each property comes from the name of a reducer //? displaying different syntax options here.
   users: usersReducer, //should be shortened to just be users because the name is the same
-  currentUser
+  currentUser,
+  loginForm
 })
 const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)))
 
