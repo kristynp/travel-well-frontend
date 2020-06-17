d3b16935357c764b5e49bf26ba31aa265d9a35d8
github username <github email address>
2020-06-17 10:25:53 -0600

    installed and imported requirements for redux, including provider, createStore, and thunk, made a mock reducer to get it set up

--- src/index.js
+++ src/index.js
@@ -1,6 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import App from './App';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 
-ReactDOM.render( <App />, document.getElementById('root'));
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+///////////
+//!
+//! don't have a reducer set up yet. This is a mock setup
+const users = () => []
+
+const reducer = combineReducers({
+  users: users //could be shortened to just be users because the name is the same
+})
+const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)))
+//////////
+
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
