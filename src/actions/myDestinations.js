c9bb961fba583b77744c24bef4430a3c78f632c2
github username <github email address>
2020-06-19 15:30:22 -0600

    created myDestinations reducer, imported it into store and combine reducers. created myDestinations action creator

---
+++ src/actions/myDestinations.js
@@ -0,0 +1,6 @@
+export const setMyDestinations = destinations => {
+  return {
+    type: "SET_MY_DESTINATIONS",
+    destinations
+  }
+}
--- src/store.js
+++ src/store.js
@@ -2,14 +2,16 @@ import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import usersReducer from './reducers/users';
 import currentUser from './reducers/currentUser';
 import loginForm from './reducers/loginForm';
+import myDestinations from './reducers/myDestinations';
 import thunk from 'redux-thunk';
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const reducer = combineReducers({
-  // each property comes from the name of a reducer //? displaying different syntax options here.
-  users: usersReducer, //should be shortened to just be users because the name is the same
+  // each property comes from the name of a reducer. Displaying different syntax options here.
+  users: usersReducer,
   currentUser,
-  loginForm
+  loginForm,
+  myDestinations
 })
 const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)))
 
