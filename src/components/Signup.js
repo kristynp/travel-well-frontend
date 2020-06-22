2708817bd45cd0071fc1f5086c0d9b05ea67795a
github username <github email address>
2020-06-22 13:25:40 -0600

    created Signup component with form

---
+++ src/components/Signup.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { updateSignupForm } from '../actions/signupForm';
+import { signup } from '../actions/currentUser';
+
+
+const Signup = ({ signupFormData, updateSignupForm, signup }) => {
+
+  const handleInputChange = event => {
+    const { name, value } = event.target
+    const updatedFormInfo = {
+      ...signupFormData,
+      [name]: value
+    }
+    updateSignupForm(updatedFormInfo)
+  }
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    signup(signupFormData)
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input name="username" type="text" placeholder="username" value={signupFormData.username} onChange={handleInputChange}/>
+      <input name="password" type="text" placeholder="password" value={signupFormData.password} onChange={handleInputChange}/>
+      <input type="submit" value="Sign Up" />
+    </form>
+  )
+}
+
+const mapStateToProps = state => {
+  return { 
+    signupFormData: state.signupForm
+  }
+}
+
+export default connect(mapStateToProps, { updateSignupForm, signup })(Signup)
