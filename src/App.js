7ab5727732115a3af435f3d228c896eb0bd0fd2b
github username <github email address>
2020-06-22 13:14:18 -0600

    moved Router wrap to index.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Login from './components/Login';
 import Logout from './components/Logout';
 import MyDestinations from './components/MyDestinations';
 import MainContainer from './components/MainContainer';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 
 class App extends React.Component {
   componentDidMount() {
@@ -15,14 +15,12 @@ class App extends React.Component {
 
   render() {
     return (
-      <Router>
-        <div className="App">
-          <NavBar/>
-          <Route exact path='/login' component={Login}/>
-          <Route exact path='/logout' component={Logout}/>
-          <Route exact path='/my-destinations' component={MyDestinations}/>
-        </div>
-      </Router>
+      <div className="App">
+        <NavBar/>
+        <Route exact path='/login' component={Login}/>
+        <Route exact path='/logout' component={Logout}/>
+        <Route exact path='/my-destinations' component={MyDestinations}/>
+      </div>
     );
   }
 
--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom'
 import store from './store'
 
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </Provider>,
   document.getElementById('root')
 );
