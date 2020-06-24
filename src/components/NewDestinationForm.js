1b1c59017b8dd94861e604952b117fdf6a066075
github username <github email address>
2020-06-24 13:05:59 -0600

    remove users from reducers and store. added handleSubmit to NewDestination form

--- src/components/NewDestinationForm.js
+++ src/components/NewDestinationForm.js
@@ -2,23 +2,29 @@ import React from 'react';
 import { updateNewDestinationForm } from '../actions/newDestinationForm';
 import { connect } from 'react-redux';
 
-const NewDestinationForm = ({}) => {
+const NewDestinationForm = ({ name, notes, history }) => {
   const handleChange = event => {
     const { name, value } = event.target
     updateNewDestinationForm(name, value)
   }
 
+  const handleSubmit = event => {
+    event.preventDefault();
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <input 
         name="name"
         onChange={handleChange}
         placeholder={"name"}
+        value={name}
       />
       <input 
         name="notes"
         onChange={handleChange}
         placeholder={"notes"}
+        value={notes}
       /> 
       <input 
         type="submit" 
--- src/store.js
+++ src/store.js
@@ -1,5 +1,4 @@
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
-import usersReducer from './reducers/users';
 import currentUser from './reducers/currentUser';
 import loginForm from './reducers/loginForm';
 import myDestinations from './reducers/myDestinations';
@@ -9,8 +8,6 @@ import thunk from 'redux-thunk';
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const reducer = combineReducers({
-  // each property comes from the name of a reducer. Displaying different syntax options here.
-  users: usersReducer,
   currentUser,
   loginForm,
   myDestinations,
