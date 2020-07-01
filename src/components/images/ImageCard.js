c74c9da04966b9ac14449b0ba3c00dfc2ed91d20
github username <kristyparker1@gmail.com>
2020-07-01 12:22:10 -0600

    image styling

--- src/components/images/ImageCard.js
+++ src/components/images/ImageCard.js
@@ -4,9 +4,9 @@ import React from 'react'
 const ImageCard = ({ image }) => {
   return (
     image ?
-    <div>
-       Image will go here
-    </div> : 
+    <span className="image-card">
+      <img className="global-image" src={image.urls.thumb} alt={image.alt_description}/>
+    </span> : 
     null 
   )
 }
