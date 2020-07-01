5b66722a54cc9a765023f0b4f137148a9a7078a9
github username <kristyparker1@gmail.com>
2020-07-01 13:38:11 -0600

    Change buttons to links in MyDestinations

--- src/components/destinations/MyDestinations.js
+++ src/components/destinations/MyDestinations.js
@@ -3,22 +3,29 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
+import { Card, CardColumns, Button } from 'react-bootstrap';
 
 const MyDestinations = props => {
   const destinationCards = props.destinations.length > 0 ? 
     props.destinations.map(d => (
-        <div key={d.id} >
-          <Link to={`/destinations/${d.id}`}>{d.attributes.name}
-          </Link>
-        </div>
+      <Card style={{ width: '18rem' }} key={d.id} >
+        <Card.Img variant="top" src="holder.js/100px180" ></Card.Img>
+        <Card.Body>
+          <Card.Title>{d.attributes.name}</Card.Title>
+          <Card.Text>Country</Card.Text>
+          <Link to={`/destinations/${d.id}`}>See Details</Link>
+        </Card.Body>
+      </Card>
 
     )) 
     : null
 
   return (
     <>
-    <h1 className="destinations-title">My Destinations</h1>
-    {destinationCards}
+      <h1 className="destinations-title">My Destinations</h1>
+      <CardColumns>
+        {destinationCards}
+      </CardColumns>
     </>
   )
 }
