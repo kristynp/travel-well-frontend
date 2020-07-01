2dab840f4b386938656ba227defd8146f9952ecb
github username <kristyparker1@gmail.com>
2020-07-01 13:52:05 -0600

    moved card render to DestinationCards component

--- src/components/destinations/DestinationCard.js
+++ src/components/destinations/DestinationCard.js
@@ -1,14 +1,20 @@
 // functional presentational component
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { Card } from 'react-bootstrap';
+
 
 const DestinationCard = ({ destination }) => {
   return (
     destination ?
-    <div>
-      <h2>{destination.attributes.name}</h2>
-      
-    </div> : 
+    <Card style={{ width: '18rem' }} key={destination.id} >
+      <Card.Img variant="top" src="holder.js/100px180" ></Card.Img>
+      <Card.Body>
+        <Card.Title>{destination.attributes.name}</Card.Title>
+        <Card.Text>Country</Card.Text>
+        <Link to={`/destinations/${destination.id}`}>See Details</Link>
+      </Card.Body>
+    </Card> : 
     null 
   )
 }
--- src/components/destinations/MyDestinations.js
+++ src/components/destinations/MyDestinations.js
@@ -2,27 +2,21 @@
 // responsible for rendering out a Destination component that will show each individual Destination
 import React from 'react'
 import { connect } from 'react-redux'
-import { Link } from 'react-router-dom'
-import { Card, CardColumns, Button } from 'react-bootstrap';
+import DestinationCard from './DestinationCard';
+import { CardColumns } from 'react-bootstrap';
+
 
 const MyDestinations = props => {
   const destinationCards = props.destinations.length > 0 ? 
     props.destinations.map(d => (
-      <Card style={{ width: '18rem' }} key={d.id} >
-        <Card.Img variant="top" src="holder.js/100px180" ></Card.Img>
-        <Card.Body>
-          <Card.Title>{d.attributes.name}</Card.Title>
-          <Card.Text>Country</Card.Text>
-          <Link to={`/destinations/${d.id}`}>See Details</Link>
-        </Card.Body>
-      </Card>
-
+      <DestinationCard destination={d}/>
     )) 
     : null
 
   return (
     <>
       <h1 className="destinations-title">My Destinations</h1>
+      <br/><br/>
       <CardColumns>
         {destinationCards}
       </CardColumns>
