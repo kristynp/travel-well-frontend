55fa85ad12aee9ce485bac4acaff1666d5d3361b
github username <kristyparker1@gmail.com>
2020-07-01 21:45:13 -0600

    add country to setFormDataForEdit action creator

--- src/actions/destinations/destinationForm.js
+++ src/actions/destinations/destinationForm.js
@@ -1,7 +1,6 @@
 //synchronous action creators
 
 export const updateDestinationForm = (name, value) => {
-  console.log(name, value)
   return {
     type: "UPDATE_DESTINATION_FORM",
     formData: { name, value }
@@ -17,6 +16,7 @@ export const resetDestinationForm = () => {
 export const setFormDataForEdit = destination => {
   const destinationFormData = {
     name: destination.attributes.name,
+    country: destination.attributes.country,
     notes: destination.attributes.notes
   }
   return {
