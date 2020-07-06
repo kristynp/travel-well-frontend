eb0eaf3417590b8e4de70336fb73c2e40efcc1cd
github username <kristyparker1@gmail.com>
2020-07-06 09:36:07 -0600

    wrap desination images in object that contains destinationId

--- src/reducers/images/destinationImages.js
+++ src/reducers/images/destinationImages.js
@@ -1,7 +1,7 @@
 export default (state = [], action) => {
   switch (action.type) {
     case "SET_DESTINATION_IMAGE_DATA":
-      return action.imageData
+      return [{destinationId: action.destinationId, images: action.imageData}]
     default:
       return state
   }
