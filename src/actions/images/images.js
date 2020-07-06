d148ad767fbd918350eee7ffdc2aded2b80fe5ee
github username <kristyparker1@gmail.com>
2020-07-06 09:35:31 -0600

    add destinationId in action for setting destination images

--- src/actions/images/images.js
+++ src/actions/images/images.js
@@ -7,12 +7,34 @@ export const setGlobalImageData = imageData => {
   }
 }
 
+export const setDestinationImageData = (imageData, id) => {
+  return {
+    type: "SET_DESTINATION_IMAGE_DATA",
+    destinationId: id,
+    imageData: imageData
+  }
+}
+
 //asynchronous action creators
 
-export const getDestinationImages = destinationCountry => {
+export const getDestinationImages = (destinationCountry, id) => {
+  const countrySlug = destinationCountry.replace(/\s/g , "-").toLowerCase()
+  console.log('countrySlug', countrySlug)
   return dispatch => {
     const access_key = process.env.REACT_APP_UNSPLASH_ACCESS_KEY
-    
+    return fetch(`https://api.unsplash.com/search/photos?page=6&query=${countrySlug}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Client-ID ${access_key}`
+      }
+    })
+    .then(resp => resp.json())
+    .then(resp => {
+      console.log('resp', resp) // array of image objects
+      dispatch(setDestinationImageData(resp.results, id))
+    })
+    .catch(console.log)
   }
 }
 
