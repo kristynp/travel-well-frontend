ea933736e28c114e3c043718da2ef0e16b1089c3
github username <kristyparker1@gmail.com>
2020-07-07 17:37:01 -0600

    change DestinationDetails to a class to make use of componentDidMount

--- src/components/destinations/DestinationDetails.js
+++ src/components/destinations/DestinationDetails.js
@@ -2,16 +2,24 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const DestinationDetails = ({ destination }) => {
-  return (
+class DestinationDetails extends React.Component {
+
+  componentDidMount() {
+    
+  }
+
+  render () {
+    const destination = this.props.destination
+    const details =     
     destination ?
-    <div>
-      <h2 className="destinations-title">{destination.attributes.name}</h2>
-      <p>Notes: {destination.attributes.notes}</p>
-      <Link to={`/destinations/${destination.id}/edit`} >Edit Destination</Link>
-    </div> : 
+      <div>
+        <h2 className="destinations-title">{destination.attributes.name}</h2>
+        <p>Notes: {destination.attributes.notes}</p>
+        <Link to={`/destinations/${destination.id}/edit`} >Edit Destination</Link>
+      </div> : 
     null 
-  )
+    return details
+  }
 }
 
 export default DestinationDetails;
