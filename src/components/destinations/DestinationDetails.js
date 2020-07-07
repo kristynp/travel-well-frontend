25193d4e6b7e1a28976e70b4344560fdaf4067ba
github username <kristyparker1@gmail.com>
2020-07-07 13:18:00 -0600

    center title

--- src/components/destinations/DestinationDetails.js
+++ src/components/destinations/DestinationDetails.js
@@ -6,7 +6,7 @@ const DestinationDetails = ({ destination }) => {
   return (
     destination ?
     <div>
-      <h2>{destination.attributes.name}</h2>
+      <h2 className="destinations-title">{destination.attributes.name}</h2>
       <p>Notes: {destination.attributes.notes}</p>
       <Link to={`/destinations/${destination.id}/edit`} >Edit Destination</Link>
     </div> : 
