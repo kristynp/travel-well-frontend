dad97ae87c35bd18adf3d933eb575cf74a3b8a6d
github username <kristyparker1@gmail.com>
2020-07-08 11:45:14 -0600

    prevent page refresh with use of Nav Links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,14 +4,15 @@ import { connect } from 'react-redux';
 import Logout from './user/Logout';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar, Nav } from 'react-bootstrap';
+import { Link } from 'react-router-dom'
 
 const NavBar = ({ currentUser, loggedIn }) => {
   return (
     <Navbar bg="dark" variant="dark">
-    <Navbar.Brand href="/">TravelWell</Navbar.Brand>
+    <Navbar.Brand as={Link} to="/">TravelWell</Navbar.Brand>
     <Nav className="mr-auto">
-      <Nav.Link href="/destinations">My Destinations</Nav.Link>
-      <Nav.Link href="/destinations/new">Add Destination</Nav.Link>
+      <Nav.Link as={Link} to="/destinations">My Destinations</Nav.Link>
+      <Nav.Link as={Link} to="/destinations/new">Add Destination</Nav.Link>
     </Nav>
       { loggedIn ? <Logout/> : null }
     </Navbar>
